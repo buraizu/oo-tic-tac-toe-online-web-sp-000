28f469cfd32049ef129f4b42c3acef75c11ea961
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-06 19:29:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,4 +12,11 @@ class TicTacToe
     [1,4,7],  # Middle Column
     [2,5,8]   # Right Column
   ]
+  def display_board(@board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
 end
